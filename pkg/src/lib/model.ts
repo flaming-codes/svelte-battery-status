d2ab9bf447d0835788ea09eeffd7adf3b915d57c
Tom Schönmann <tom@wild.as>
2022-11-19 11:27:43 +0100

    chore: update env-usage

--- pkg/src/lib/model.ts
+++ pkg/src/lib/model.ts
@@ -1,4 +1,4 @@
-import { browser } from "$app/env";
+import { browser } from "$app/environment";
 import type { Subscriber } from "svelte/store";
 import type { BatteryManger } from "../app";
 import type { BatteryStore } from "./types";
